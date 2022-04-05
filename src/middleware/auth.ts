267462d624466cb6d3a6f5e109197d5719baa45a
harelk1015 <harelk1015@gmail.com>
2022-04-06 02:13:32 +0300

    first commit

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -23,15 +23,19 @@ const auth: RequestHandler = async (req: IAuthMiddlewareRequest, res, next) => {
   let userDocument: Readonly<Omit<mongoose.Document, '_id'>> | null;
 
   try {
+    // Gets the token
     const token = (req.header('Authorization') as string).replace(
       'Bearer ',
       ''
     );
 
+    // Verfiy the token and get the userId --- data = token that has the user id
     data = jwt.verify(token, 'secret') as IVerify;
 
+    // Get the user object that matches the ID recived from the verification
     userDocument = await UserDB.findById(data._id, { _id: 0 });
 
+    // Passes the user object for the later adminAuth auth
     req.user = userDocument as IUser;
 
     if (!userDocument) {
@@ -40,12 +44,14 @@ const auth: RequestHandler = async (req: IAuthMiddlewareRequest, res, next) => {
 
     next();
   } catch (e: any) {
-    // user failes auth and now checking for validity of refresh token
 
+    // User failes auth and now checking for validity of refresh token
     const refreshToken = (req.header('AuthorizationRefresh') as string).replace(
       'Bearer ',
       ''
     );
+
+    // Checks if there is refreshToken on the headers
 
     if (refreshToken == null) {
       return next(
@@ -53,28 +59,35 @@ const auth: RequestHandler = async (req: IAuthMiddlewareRequest, res, next) => {
       );
     }
 
+    // Verfiy the token and get the userId --- data = token that has the user id
     data = jwt.verify(refreshToken, 'secret') as IVerify;
 
+    // Get the user object that matches the ID recived from the verification
     const user = await UserDB.findById(data._id, { _id: 0 });
 
+    // Passes the user object for the later adminAuth auth
     req.user = user;
 
     if (user) {
       let userTokens: string[] = [];
-
+      
       user.tokens.map((token) => {
         userTokens.push(token.token);
       });
-
+    
+      // Checks if the current refreshToken of the user is included in the 5 latest refreshTokens
       if (!userTokens.includes(refreshToken)) {
         return next(new HttpError('refresh token too old, login again', 401));
       }
     }
 
+    // Gets the id of the verified user inorder to generate new access token
     const verifiedUserId = jwt.verify(refreshToken, 'secret') as IVerify;
 
+    // Generates new access token
     const accessToken = generateAccessToken(verifiedUserId._id);
 
+    // Passes the new token to the req 
     req.accessToken = accessToken;
 
     next();
