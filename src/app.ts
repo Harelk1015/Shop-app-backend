ed88e9d34e46e929cf26f79baff0d522b5243c98
harelk1015 <harelk1015@gmail.com>
2022-04-09 12:35:37 +0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -7,21 +7,21 @@ import authRoutes from './routes/auth.router';
 import productRouter from './routes/product.router';
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: "*"}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization,AuthorizationRefresh, user'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization ,AuthorizationRefresh ,user'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Origin', '*');
 
   next();
 });
@@ -43,7 +43,6 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.json({ message: error.message || 'an unknown error occurred' });
 });
 
-('mongodb+srv://user:ySuVZNb4Jq90JRg3@cluster0.1szjm.mongodb.net/shopApp');
 
 app.listen(3030, () => {
   console.log('server is running');
