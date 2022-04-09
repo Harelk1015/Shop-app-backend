c661e4dc47d5dca4446b113780ea1a22d09105db
harelk1015 <harelk1015@gmail.com>
2022-04-09 13:01:07 +0300

    change

--- src/app.ts
+++ src/app.ts
@@ -12,21 +12,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization ,AuthorizationRefresh ,user'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PATCH, DELETE'
-//   );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin:', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers:',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization ,AuthorizationRefresh ,user'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods:',
+    'OPTIONS, GET, POST, PATCH, DELETE'
+  );
 
-//   next();
-// });
+  next();
+});
 
-('Authorization ,AuthorizationRefresh, user');
+
 
 app.use('/auth', authRoutes);
 app.use('/products', productRouter);
