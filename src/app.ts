e35289d7a2d64586d218f1d317fd4c014cdd8e7a
harelk1015 <harelk1015@gmail.com>
2022-04-09 13:14:20 +0300

    change

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import authRoutes from './routes/auth.router';
 import productRouter from './routes/product.router';
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
