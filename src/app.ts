469c6cff96d346123b6fce0c4989ec01b3940603
harelk1015 <harelk1015@gmail.com>
2022-04-09 13:26:21 +0300

    change

--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,24 @@ import authRoutes from './routes/auth.router';
 import productRouter from './routes/product.router';
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization ,AuthorizationRefresh ,user'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PATCH, DELETE'
-//   );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization ,AuthorizationRefresh ,user'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PATCH, DELETE'
+  );
 
-//   next();
-// });
+  next();
+});
 
 
 
